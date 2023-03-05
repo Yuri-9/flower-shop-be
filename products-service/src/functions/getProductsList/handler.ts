6752ff16250c0f113acf12d172232351a177bbd9
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-03-05 16:15:30 +0100

    feat/connect getProductsList to dynamoDB

--- products-service/src/functions/getProductsList/handler.ts
+++ products-service/src/functions/getProductsList/handler.ts
@@ -1,13 +1,26 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { successResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-import { productList } from '@model/data-product-list';
 import { Product } from '@model/index';
+import { dataBase, getScanParams } from '@db/db';
+import { arrayToMapByField } from '@utils/arrayToMapByField.js';
 
 import schema from './schema';
 
+const { TABLE_NAME_PRODUCT, TABLE_NAME_STOCK } = process.env;
+
 export const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  return successResponse<Product[]>(productList);
+  const products = await dataBase.scan(getScanParams(TABLE_NAME_PRODUCT));
+  const productsStock = await dataBase.scan(getScanParams(TABLE_NAME_STOCK));
+
+  const productsStockMapById = arrayToMapByField('product_id', productsStock.Items);
+
+  const productsListFullData: Product[] = products.Items?.map((product) => ({
+    ...product,
+    count: productsStockMapById.get(product.id)?.count,
+  }));
+
+  return successResponse<Product[]>(productsListFullData);
 };
 
 export const main = middyfy(getProductsList);
