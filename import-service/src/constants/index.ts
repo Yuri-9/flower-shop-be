0ce55f5422e4dcaa35d3453d60c7b7ff3eb755f9
Yury Vashkevich <yury_vashkevich1@epam.com>
2023-03-11 20:47:34 +0100

    feat/ implement importFileParser

--- import-service/src/constants/index.ts
+++ import-service/src/constants/index.ts
@@ -1,6 +1,7 @@
 export const REGION = "eu-west-1";
 export const BUCKET_NAME = "import-service-flower";
 export enum FolderName {
-  UPLOADED = "uploaded",
   FILES = "files",
+  UPLOADED = "uploaded",
+  PARSED = "parsed",
 }
--- import-service/src/functions/importFileParser/index.ts
+++ import-service/src/functions/importFileParser/index.ts
@@ -1,17 +1,20 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import csvParser from "csv-parser";
-import { REGION } from "src/constants";
+import { BUCKET_NAME, FolderName, REGION } from "src/constants";
 import { asStream } from "src/types";
+import s3ObjectService from "src/services/s3ObjectService";
+import AWS from "aws-sdk";
 
 export const importFileParser = async (event) => {
   const client = new S3Client({ region: REGION });
+  const s3 = new AWS.S3({ region: REGION });
 
-  console.log(" -- event", event);
+  console.log(" -- event.Records", JSON.stringify(event.Records));
 
   try {
     for await (const record of event.Records) {
       const { name } = record.s3.bucket;
-      const { key } = record.s3.object;
+      const { key }: { key: string } = record.s3.object;
 
       // Read file
       const response = await client.send(
@@ -27,6 +30,18 @@ export const importFileParser = async (event) => {
       for await (const record of stream) {
         console.log(" -- Product: ", record);
       }
+
+      await s3ObjectService.copyObject(s3, {
+        bucketName: BUCKET_NAME,
+        objectKey: key,
+        prefixOld: FolderName.UPLOADED,
+        prefixNew: FolderName.PARSED,
+      });
+
+      await s3ObjectService.deleteObject(s3, {
+        bucketName: BUCKET_NAME,
+        objectKey: key,
+      });
     }
   } catch (err) {
     console.log(` -- Error importFileParser: ${err} `);
